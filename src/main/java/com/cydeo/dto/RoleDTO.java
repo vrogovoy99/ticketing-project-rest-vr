90bab3e3aded2104393971caf8053675a2430c4a
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-06-18 10:40:23 +0200

    removes validation from DTOs

--- src/main/java/com/cydeo/dto/RoleDTO.java
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -1,12 +1,12 @@
 package com.cydeo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
 public class RoleDTO {
 
     private Long id;
