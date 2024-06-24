4ad4e0df39bdcd26f490011296ecb10253fda8ac
Human <vladislav.rogovoy@gmail.com>
2024-06-24 10:05:15 -0400

    keycloak services added

--- src/main/java/com/cydeo/service/KeycloakService.java
+++ src/main/java/com/cydeo/service/KeycloakService.java
@@ -4,7 +4,7 @@ import com.cydeo.dto.UserDTO;
 
 import javax.ws.rs.core.Response;
 
-public interface KeyCloakService {
+public interface KeycloakService {
     Response userCreate(UserDTO userDTO);
 
     void delete(String userName);
